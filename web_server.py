e2974da05d18397d0dc2c63b932225ba7a2dd236
Bryan <bryanjw01@gmail.com>
2019-10-09 02:32:19 -0400

    web server using bottle - will change it to flask in the future

---
+++ web_server.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+import logging
+import os
+
+import bottle
+import grpc
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+import server_diva_pb2
+import server_diva_pb2_grpc
+
+directory = os.getcwd()
+app = bottle.Bottle()
+
+# Change many values at once
+app.config.update({
+    'autojson': False,
+    'sqlite.db': ':memory:',
+    'myapp.param': 'value',
+    'app.host': 'localhost',
+    'app.port': 10000,
+})
+
+
+@app.hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+    bottle.response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    bottle.response.headers[
+        'Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+
+@app.get('/<filename:path>', method="GET")  # ":path, the filter as path format"
+def server_static(filename):
+    return bottle.static_file(filename, root=directory)
+
+
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/')
+def index():
+    return bottle.static_file('./template/index.html', root=directory)
+
+
+@app.route('/begin', method="POST")
+def query():
+    logging.basicConfig()
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    print("Greeter client received: " + response.message)
+
+
+@app.route('/display', method="POST")
+def query():
+    print("good")
+    logging.basicConfig()
+    with grpc.insecure_channel('localhost:50052') as channel:
+        stub = server_diva_pb2_grpc.server_divaStub(channel)
+        response = stub.request_frame_path(server_diva_pb2.query_statement(name='requested directory for frames'))
+    pic_files = list()
+    if os.path.exists(response.directory_path):
+        temp = os.listdir(response.directory_path)
+        for files in temp:
+            if 'png' or 'jpeg' in files:
+                pic_files.append(files)
+    temp = ','.join(pic_files)
+    return temp
+
+
+if __name__ == '__main__':
+    app.config.load_config('conf.ini')
+    bottle.run(app, host='localhost', port=10000, debug=True)
