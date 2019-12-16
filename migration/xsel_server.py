b6784ce5d2261b455e33a2d8fad47c3345fa1a2a
swhsiang <swh@hsiang.io>
2019-12-17 01:04:30 +0800

    Fix logging message format error

--- migration/xsel_server.py
+++ migration/xsel_server.py
@@ -14,9 +14,10 @@ video_list = [('sonic.mp4', os.path.join(os.curdir, VIDEO_FOLDER,
               ('example.mp4',
                os.path.join(os.curdir, VIDEO_FOLDER, 'example.mp4'))]
 
-FORMAT = '%(asctime)-15s %(thread)d %(threadName)s %(user)-8s %(message)s'
+FORMAT = '%(asctime)-15s %(thread)d %(threadName)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
 logger = logging.getLogger(__name__)
+logging.getLogger('sqlalchemy').setLevel(logging.INFO)
 
 logger.info("Begin to initialize DB")
 
