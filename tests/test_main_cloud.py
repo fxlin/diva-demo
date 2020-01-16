c1a5cf0cce30c30c6e38f0c13b64484251a065ac
swhsiang <swh@hsiang.io>
2020-01-16 11:42:57 -0500

    Import unittest lib and update tests/test_main_cloud.py based on the change

--- tests/test_main_cloud.py
+++ tests/test_main_cloud.py
@@ -1,3 +1,4 @@
+import unittest
 import logging
 import time
 
@@ -27,11 +28,14 @@ def query_video(object_name: str, video_name: str):
     session.remove()
     return res[0].id
 
+
 def detect_object(object_name: str, video_name: str, video_id: int):
     with grpc.insecure_channel(DIVA_CHANNEL_ADDRESS) as channel:
         stub = server_diva_pb2_grpc.server_divaStub(channel)
-        _ = stub.detect_object_in_video(server_diva_pb2.object_video_pair(object_name=object_name, video_name=video_name))
-    
+        _ = stub.detect_object_in_video(
+            server_diva_pb2.object_video_pair(object_name=object_name,
+                                              video_name=video_name))
+
     time.sleep(3)
 
     session = db_session()
@@ -42,7 +46,6 @@ def detect_object(object_name: str, video_name: str, video_id: int):
     _temp = session.query(Element).all()
 
     return _temp
-
 
 
 def simulate_detection():
@@ -57,3 +60,20 @@ def simulate_detection():
     print(elements)
 
     return elements
+
+
+class TestObjectDetection(unittest.TestCase):
+    OBJECT_NAME = 'bike'
+    VIDEO_NAME = 'traffic_cam_vid.mp4'
+
+    def test_query_video(self):
+        try:
+            temp_res = query_video(self.OBJECT_NAME, self.VIDEO_NAME)
+        except Exception as err:
+            self.fail(f'calling query_video but got {err}')
+
+        self.assertEqual(2, temp_res, 'id of traffic_cam_vid.mp4 should be 2')
+
+
+if __name__ == "__main__":
+    unittest.main()
