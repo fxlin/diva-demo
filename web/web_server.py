b97a9eae53672392b47c9c41cd735aed6734bd87
Bryan <bryanjw01@gmail.com>
2020-01-24 16:16:11 -0500

    updated web_server.py

--- web/web_server.py
+++ web/web_server.py
@@ -28,9 +28,11 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html')
 
+
 @app.route('/uploads/<path:filename>')
 def download_file(filename):
     return send_from_directory(OUTPUT_DIR, filename, as_attachment=True)
+
 
 @app.route('/display', methods=['GET', 'POST'])
 def display():
@@ -41,10 +43,8 @@ def display():
     with grpc.insecure_channel(DIVA_CHANNEL_ADDRESS) as channel:
         stub = server_diva_pb2_grpc.server_divaStub(channel)
         #  response = stub.request_frame_path(server_diva_pb2.query_statement(name='request directory'))
-        _= stub.detect_object_in_video(server_diva_pb2.object_video_pair(object_name=obj,  video_name=video))
-    temp = 'good'
-    print(temp)
-    return jsonify(file=temp)
+        _ = stub.detect_object_in_video(server_diva_pb2.object_video_pair(object_name=obj,  video_name=video))
+    return jsonify(file='good')
 
 
 @app.route('/retrieve', methods=['GET', 'POST'])
