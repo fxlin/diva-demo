56e4eafc52ec0e64cc67736e51549eee7aebe9db
swhsiang <swh@hsiang.io>
2020-04-19 23:44:48 -0400

    update proto definition 2

--- det_yolov3_pb2_grpc.py
+++ det_yolov3_pb2_grpc.py
@@ -5,59 +5,92 @@ import det_yolov3_pb2 as det__yolov3__pb2
 
 
 class DetYOLOv3Stub(object):
-  # missing associated documentation comment in .proto file
-  pass
+    """Missing associated documentation comment in .proto file"""
 
-  def __init__(self, channel):
-    """Constructor.
+    def __init__(self, channel):
+        """Constructor.
 
-    Args:
-      channel: A grpc.Channel.
-    """
-    self.DetFrame = channel.unary_unary(
-        '/det_yolov3.DetYOLOv3/DetFrame',
-        request_serializer=det__yolov3__pb2.DetFrameRequest.SerializeToString,
-        response_deserializer=det__yolov3__pb2.Score.FromString,
-        )
-    self.Detect = channel.unary_unary(
-        '/det_yolov3.DetYOLOv3/Detect',
-        request_serializer=det__yolov3__pb2.DetectionRequest.SerializeToString,
-        response_deserializer=det__yolov3__pb2.DetectionOutput.FromString,
-        )
+        Args:
+            channel: A grpc.Channel.
+        """
+        self.DetFrame = channel.unary_unary(
+                '/det_yolov3.DetYOLOv3/DetFrame',
+                request_serializer=det__yolov3__pb2.DetFrameRequest.SerializeToString,
+                response_deserializer=det__yolov3__pb2.Score.FromString,
+                )
+        self.Detect = channel.unary_unary(
+                '/det_yolov3.DetYOLOv3/Detect',
+                request_serializer=det__yolov3__pb2.DetectionRequest.SerializeToString,
+                response_deserializer=det__yolov3__pb2.DetectionOutput.FromString,
+                )
 
 
 class DetYOLOv3Servicer(object):
-  # missing associated documentation comment in .proto file
-  pass
+    """Missing associated documentation comment in .proto file"""
 
-  def DetFrame(self, request, context):
-    # missing associated documentation comment in .proto file
-    pass
-    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-    context.set_details('Method not implemented!')
-    raise NotImplementedError('Method not implemented!')
+    def DetFrame(self, request, context):
+        """Missing associated documentation comment in .proto file"""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
-  def Detect(self, request, context):
-    # missing associated documentation comment in .proto file
-    pass
-    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-    context.set_details('Method not implemented!')
-    raise NotImplementedError('Method not implemented!')
+    def Detect(self, request, context):
+        """Missing associated documentation comment in .proto file"""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
 
 def add_DetYOLOv3Servicer_to_server(servicer, server):
-  rpc_method_handlers = {
-      'DetFrame': grpc.unary_unary_rpc_method_handler(
-          servicer.DetFrame,
-          request_deserializer=det__yolov3__pb2.DetFrameRequest.FromString,
-          response_serializer=det__yolov3__pb2.Score.SerializeToString,
-      ),
-      'Detect': grpc.unary_unary_rpc_method_handler(
-          servicer.Detect,
-          request_deserializer=det__yolov3__pb2.DetectionRequest.FromString,
-          response_serializer=det__yolov3__pb2.DetectionOutput.SerializeToString,
-      ),
-  }
-  generic_handler = grpc.method_handlers_generic_handler(
-      'det_yolov3.DetYOLOv3', rpc_method_handlers)
-  server.add_generic_rpc_handlers((generic_handler,))
+    rpc_method_handlers = {
+            'DetFrame': grpc.unary_unary_rpc_method_handler(
+                    servicer.DetFrame,
+                    request_deserializer=det__yolov3__pb2.DetFrameRequest.FromString,
+                    response_serializer=det__yolov3__pb2.Score.SerializeToString,
+            ),
+            'Detect': grpc.unary_unary_rpc_method_handler(
+                    servicer.Detect,
+                    request_deserializer=det__yolov3__pb2.DetectionRequest.FromString,
+                    response_serializer=det__yolov3__pb2.DetectionOutput.SerializeToString,
+            ),
+    }
+    generic_handler = grpc.method_handlers_generic_handler(
+            'det_yolov3.DetYOLOv3', rpc_method_handlers)
+    server.add_generic_rpc_handlers((generic_handler,))
+
+
+ # This class is part of an EXPERIMENTAL API.
+class DetYOLOv3(object):
+    """Missing associated documentation comment in .proto file"""
+
+    @staticmethod
+    def DetFrame(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/det_yolov3.DetYOLOv3/DetFrame',
+            det__yolov3__pb2.DetFrameRequest.SerializeToString,
+            det__yolov3__pb2.Score.FromString,
+            options, channel_credentials,
+            call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def Detect(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/det_yolov3.DetYOLOv3/Detect',
+            det__yolov3__pb2.DetectionRequest.SerializeToString,
+            det__yolov3__pb2.DetectionOutput.FromString,
+            options, channel_credentials,
+            call_credentials, compression, wait_for_ready, timeout, metadata)
--- server_diva_pb2_grpc.py
+++ server_diva_pb2_grpc.py
@@ -7,110 +7,190 @@ import server_diva_pb2 as server__diva__pb2
 
 
 class server_divaStub(object):
-  # missing associated documentation comment in .proto file
-  pass
+    """Missing associated documentation comment in .proto file"""
 
-  def __init__(self, channel):
-    """Constructor.
+    def __init__(self, channel):
+        """Constructor.
 
-    Args:
-      channel: A grpc.Channel.
-    """
-    self.detect_object_in_video = channel.unary_unary(
-        '/server_diva.server_diva/detect_object_in_video',
-        request_serializer=server__diva__pb2.object_video_pair.SerializeToString,
-        response_deserializer=server__diva__pb2.detection_result.FromString,
-        )
-    self.register_camera = channel.unary_unary(
-        '/server_diva.server_diva/register_camera',
-        request_serializer=server__diva__pb2.camera_info.SerializeToString,
-        response_deserializer=server__diva__pb2.response.FromString,
-        )
-    self.detect_object_in_frame = channel.unary_unary(
-        '/server_diva.server_diva/detect_object_in_frame',
-        request_serializer=server__diva__pb2.frame_from_camera.SerializeToString,
-        response_deserializer=server__diva__pb2.response.FromString,
-        )
-    self.process_video = channel.unary_unary(
-        '/server_diva.server_diva/process_video',
-        request_serializer=common__pb2.VideoRequest.SerializeToString,
-        response_deserializer=server__diva__pb2.response.FromString,
-        )
-    self.get_videos = channel.unary_unary(
-        '/server_diva.server_diva/get_videos',
-        request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
-        response_deserializer=common__pb2.get_videos_resp.FromString,
-        )
+        Args:
+            channel: A grpc.Channel.
+        """
+        self.detect_object_in_video = channel.unary_unary(
+                '/server_diva.server_diva/detect_object_in_video',
+                request_serializer=server__diva__pb2.object_video_pair.SerializeToString,
+                response_deserializer=server__diva__pb2.detection_result.FromString,
+                )
+        self.register_camera = channel.unary_unary(
+                '/server_diva.server_diva/register_camera',
+                request_serializer=server__diva__pb2.camera_info.SerializeToString,
+                response_deserializer=server__diva__pb2.response.FromString,
+                )
+        self.detect_object_in_frame = channel.unary_unary(
+                '/server_diva.server_diva/detect_object_in_frame',
+                request_serializer=server__diva__pb2.frame_from_camera.SerializeToString,
+                response_deserializer=server__diva__pb2.response.FromString,
+                )
+        self.process_video = channel.unary_unary(
+                '/server_diva.server_diva/process_video',
+                request_serializer=common__pb2.VideoRequest.SerializeToString,
+                response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                )
+        self.get_videos = channel.unary_unary(
+                '/server_diva.server_diva/get_videos',
+                request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+                response_deserializer=common__pb2.get_videos_resp.FromString,
+                )
 
 
 class server_divaServicer(object):
-  # missing associated documentation comment in .proto file
-  pass
+    """Missing associated documentation comment in .proto file"""
 
-  def detect_object_in_video(self, request, context):
-    """deprecated
-    """
-    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-    context.set_details('Method not implemented!')
-    raise NotImplementedError('Method not implemented!')
+    def detect_object_in_video(self, request, context):
+        """deprecated
+        """
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
-  def register_camera(self, request, context):
-    # missing associated documentation comment in .proto file
-    pass
-    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-    context.set_details('Method not implemented!')
-    raise NotImplementedError('Method not implemented!')
+    def register_camera(self, request, context):
+        """Missing associated documentation comment in .proto file"""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
-  def detect_object_in_frame(self, request, context):
-    # missing associated documentation comment in .proto file
-    pass
-    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-    context.set_details('Method not implemented!')
-    raise NotImplementedError('Method not implemented!')
+    def detect_object_in_frame(self, request, context):
+        """Missing associated documentation comment in .proto file"""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
-  def process_video(self, request, context):
-    """maintaining
-    """
-    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-    context.set_details('Method not implemented!')
-    raise NotImplementedError('Method not implemented!')
+    def process_video(self, request, context):
+        """maintaining
+        """
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
-  def get_videos(self, request, context):
-    # missing associated documentation comment in .proto file
-    pass
-    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-    context.set_details('Method not implemented!')
-    raise NotImplementedError('Method not implemented!')
+    def get_videos(self, request, context):
+        """Missing associated documentation comment in .proto file"""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
 
 def add_server_divaServicer_to_server(servicer, server):
-  rpc_method_handlers = {
-      'detect_object_in_video': grpc.unary_unary_rpc_method_handler(
-          servicer.detect_object_in_video,
-          request_deserializer=server__diva__pb2.object_video_pair.FromString,
-          response_serializer=server__diva__pb2.detection_result.SerializeToString,
-      ),
-      'register_camera': grpc.unary_unary_rpc_method_handler(
-          servicer.register_camera,
-          request_deserializer=server__diva__pb2.camera_info.FromString,
-          response_serializer=server__diva__pb2.response.SerializeToString,
-      ),
-      'detect_object_in_frame': grpc.unary_unary_rpc_method_handler(
-          servicer.detect_object_in_frame,
-          request_deserializer=server__diva__pb2.frame_from_camera.FromString,
-          response_serializer=server__diva__pb2.response.SerializeToString,
-      ),
-      'process_video': grpc.unary_unary_rpc_method_handler(
-          servicer.process_video,
-          request_deserializer=common__pb2.VideoRequest.FromString,
-          response_serializer=server__diva__pb2.response.SerializeToString,
-      ),
-      'get_videos': grpc.unary_unary_rpc_method_handler(
-          servicer.get_videos,
-          request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
-          response_serializer=common__pb2.get_videos_resp.SerializeToString,
-      ),
-  }
-  generic_handler = grpc.method_handlers_generic_handler(
-      'server_diva.server_diva', rpc_method_handlers)
-  server.add_generic_rpc_handlers((generic_handler,))
+    rpc_method_handlers = {
+            'detect_object_in_video': grpc.unary_unary_rpc_method_handler(
+                    servicer.detect_object_in_video,
+                    request_deserializer=server__diva__pb2.object_video_pair.FromString,
+                    response_serializer=server__diva__pb2.detection_result.SerializeToString,
+            ),
+            'register_camera': grpc.unary_unary_rpc_method_handler(
+                    servicer.register_camera,
+                    request_deserializer=server__diva__pb2.camera_info.FromString,
+                    response_serializer=server__diva__pb2.response.SerializeToString,
+            ),
+            'detect_object_in_frame': grpc.unary_unary_rpc_method_handler(
+                    servicer.detect_object_in_frame,
+                    request_deserializer=server__diva__pb2.frame_from_camera.FromString,
+                    response_serializer=server__diva__pb2.response.SerializeToString,
+            ),
+            'process_video': grpc.unary_unary_rpc_method_handler(
+                    servicer.process_video,
+                    request_deserializer=common__pb2.VideoRequest.FromString,
+                    response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+            ),
+            'get_videos': grpc.unary_unary_rpc_method_handler(
+                    servicer.get_videos,
+                    request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                    response_serializer=common__pb2.get_videos_resp.SerializeToString,
+            ),
+    }
+    generic_handler = grpc.method_handlers_generic_handler(
+            'server_diva.server_diva', rpc_method_handlers)
+    server.add_generic_rpc_handlers((generic_handler,))
+
+
+ # This class is part of an EXPERIMENTAL API.
+class server_diva(object):
+    """Missing associated documentation comment in .proto file"""
+
+    @staticmethod
+    def detect_object_in_video(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/server_diva.server_diva/detect_object_in_video',
+            server__diva__pb2.object_video_pair.SerializeToString,
+            server__diva__pb2.detection_result.FromString,
+            options, channel_credentials,
+            call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def register_camera(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/server_diva.server_diva/register_camera',
+            server__diva__pb2.camera_info.SerializeToString,
+            server__diva__pb2.response.FromString,
+            options, channel_credentials,
+            call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def detect_object_in_frame(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/server_diva.server_diva/detect_object_in_frame',
+            server__diva__pb2.frame_from_camera.SerializeToString,
+            server__diva__pb2.response.FromString,
+            options, channel_credentials,
+            call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def process_video(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/server_diva.server_diva/process_video',
+            common__pb2.VideoRequest.SerializeToString,
+            google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+            options, channel_credentials,
+            call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def get_videos(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/server_diva.server_diva/get_videos',
+            google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+            common__pb2.get_videos_resp.FromString,
+            options, channel_credentials,
+            call_credentials, compression, wait_for_ready, timeout, metadata)
