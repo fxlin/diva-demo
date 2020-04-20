6095dc295f65d2d9c0ad81a5155e62f4ab0e47ed
swhsiang <swh@hsiang.io>
2020-04-20 00:14:07 -0400

    comment unused code

--- main_cloud.py
+++ main_cloud.py
@@ -60,55 +60,54 @@ FORMAT = '%(asctime)-15s %(levelname)8s %(thread)d %(threadName)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=FORMAT)
 logger = logging.getLogger(__name__)
 
+# class ImageProcessor(threading.Thread):
+#     def run(self):
+#         while not SHUTDOWN_SIGNAL.is_set():
+#             # solution for race condition:
+#             # https://stackoverflow.com/questions/44219288/
+#             # should-i-bother-locking-the-queue-when-i-put-to-or-get-from-it/44219646
+#             logger.info('working on the given image')
 
-class ImageProcessor(threading.Thread):
-    def run(self):
-        while not SHUTDOWN_SIGNAL.is_set():
-            # solution for race condition:
-            # https://stackoverflow.com/questions/44219288/
-            # should-i-bother-locking-the-queue-when-i-put-to-or-get-from-it/44219646
-            logger.info('working on the given image')
+#             self.consume_image_task()
 
-    #         self.consume_image_task()
+#     def consume_image_task(self):
+#         task = ImageQueue.get(block=True)
 
-    # def consume_image_task(self):
-    #     task = ImageQueue.get(block=True)
+#         try:
+#             image_name = task[0]
+#             image_data = task[1]
+#             bounding_boxes = task[2]
+#             self.process_frame(image_name, image_data, bounding_boxes)
+#         except Exception as err:
+#             logger.error(f'Task {task} failed. {err}')
 
-    #     try:
-    #         image_name = task[0]
-    #         image_data = task[1]
-    #         bounding_boxes = task[2]
-    #         self.process_frame(image_name, image_data, bounding_boxes)
-    #     except Exception as err:
-    #         logger.error(f'Task {task} failed. {err}')
+#     @staticmethod
+#     def process_frame(img_name: str, img_data,
+#                       res_items: List[Tuple[int, int, int, int]]):
+#         """
+#         Take two arguments: one is the image frame data and the other is the
+#         response from YOLO agent. Draw boxes around the object of interest.
+#         """
+#         if not res_items:
+#             return
 
-    # @staticmethod
-    # def process_frame(img_name: str, img_data,
-    #                   res_items: List[Tuple[int, int, int, int]]):
-    #     """
-    #     Take two arguments: one is the image frame data and the other is the
-    #     response from YOLO agent. Draw boxes around the object of interest.
-    #     """
-    #     if not res_items:
-    #         return
+#         # FIXME frame is retrived from openCV
+#         # reference https://github.com/YunYang1994/TensorFlow2.0-Examples/
+#         # blob/master/4-Object_Detection/YOLOV3/video_demo.py
+#         # image = utils.draw_bbox(frame, res_items)
+#         # result = np.asarray(image)
 
-    #     # FIXME frame is retrived from openCV
-    #     # reference https://github.com/YunYang1994/TensorFlow2.0-Examples/
-    #     # blob/master/4-Object_Detection/YOLOV3/video_demo.py
-    #     # image = utils.draw_bbox(frame, res_items)
-    #     # result = np.asarray(image)
+#         img = np.asarray(img_data)
+#         for item in res_items:
+#             x1, y1, x2, y2 = item[0], item[1], item[2], item[3]
+#             img = draw_box(img, x1, y1, x2, y2)
 
-    #     img = np.asarray(img_data)
-    #     for item in res_items:
-    #         x1, y1, x2, y2 = item[0], item[1], item[2], item[3]
-    #         img = draw_box(img, x1, y1, x2, y2)
+#         # result = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-    #     # result = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+#         cv2.imwrite(img_name, img)
 
-    #     cv2.imwrite(img_name, img)
-
-    #     del img_data
-    #     del img
+#         del img_data
+#         del img
 
 
 class ImageDoesNotExistException(Exception):
@@ -161,173 +160,171 @@ class ImageDoesNotExistException(Exception):
 
 #     db_session.remove()
 
+# class FrameProcessor(threading.Thread):
+#     def run(self):
+#         while not SHUTDOWN_SIGNAL.is_set():
+#             logger.info("Got a task to do")
+#             self.detect_object()
 
-class FrameProcessor(threading.Thread):
-    def run(self):
-        while not SHUTDOWN_SIGNAL.is_set():
-            logger.info("Got a task to do")
-            self.detect_object()
+#     @staticmethod
+#     def video_frame(video_path: str) -> int:
+#         if not os.path.exists(video_path):
+#             raise ValueError(f"path {video_path} does not exist")
 
-    # @staticmethod
-    # def video_frame(video_path: str) -> int:
-    #     if not os.path.exists(video_path):
-    #         raise ValueError(f"path {video_path} does not exist")
+#         cap = cv2.VideoCapture(video_path)
+#         # cap.get(cv2.cv.CV_CAP_PROP_FPS)
+#         res = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+#         cap.release()
+#         return res
 
-    #     cap = cv2.VideoCapture(video_path)
-    #     # cap.get(cv2.cv.CV_CAP_PROP_FPS)
-    #     res = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    #     cap.release()
-    #     return res
+#     @staticmethod
+#     def extract_one_frame(in_filename: str, frame_num: int) -> np.ndarray:
+#         if not os.path.exists(in_filename):
+#             raise ValueError(f'video does not exist: {in_filename}')
 
-    # @staticmethod
-    # def extract_one_frame(in_filename: str, frame_num: int) -> np.ndarray:
-    #     if not os.path.exists(in_filename):
-    #         raise ValueError(f'video does not exist: {in_filename}')
+#         total_frames = FrameProcessor.video_frame(in_filename)
+#         if frame_num < 0 or frame_num > total_frames:
+#             err_msg = f'frame {frame_num} is out of range [0, {total_frames}]'
+#             raise Exception(err_msg)
 
-    #     total_frames = FrameProcessor.video_frame(in_filename)
-    #     if frame_num < 0 or frame_num > total_frames:
-    #         err_msg = f'frame {frame_num} is out of range [0, {total_frames}]'
-    #         raise Exception(err_msg)
+#         cap = cv2.VideoCapture(in_filename)
+#         if not cap.isOpened():
+#             raise Exception(f'video {in_filename} is not open yet')
 
-    #     cap = cv2.VideoCapture(in_filename)
-    #     if not cap.isOpened():
-    #         raise Exception(f'video {in_filename} is not open yet')
+#         # set frame position
+#         cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+#         ret, frame = cap.read()
+#         cap.release()
 
-    #     # set frame position
-    #     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-    #     ret, frame = cap.read()
-    #     cap.release()
+#         if not ret:
+#             raise ImageDoesNotExistException(
+#                 f'Cannot not extract {frame_num}th frame from {in_filename}')
 
-    #     if not ret:
-    #         raise ImageDoesNotExistException(
-    #             f'Cannot not extract {frame_num}th frame from {in_filename}')
+#         return frame
 
-    #     return frame
+#     @staticmethod
+#     def extract_frame_nums(video_path: str) -> 'List[int]':
+#         num_of_frames = FrameProcessor.video_frame(video_path)
 
-    # @staticmethod
-    # def extract_frame_nums(video_path: str) -> 'List[int]':
-    #     num_of_frames = FrameProcessor.video_frame(video_path)
+#         index = 0
+#         frame_list = []
+#         while index < num_of_frames:
+#             frame_list.append(index)
+#             index += 10
+#         return frame_list
 
-    #     index = 0
-    #     frame_list = []
-    #     while index < num_of_frames:
-    #         frame_list.append(index)
-    #         index += 10
-    #     return frame_list
+#     @staticmethod
+#     def get_bounding_boxes(yolo_result: str
+#                            ) -> 'List[Tuple[int, int, int, int]]':
+#         if not yolo_result or len(yolo_result) == 0:
+#             return []
 
-    # @staticmethod
-    # def get_bounding_boxes(yolo_result: str
-    #                        ) -> 'List[Tuple[int, int, int, int]]':
-    #     if not yolo_result or len(yolo_result) == 0:
-    #         return []
+#         res_items = yolo_result.split('|')
+#         res_item_coordinates = map(lambda x: x.split(','), res_items)
+#         res_items_end = list(
+#             filter(lambda z: float(z[0]) > YOLO_SCORE_THRE,
+#                    res_item_coordinates))
 
-    #     res_items = yolo_result.split('|')
-    #     res_item_coordinates = map(lambda x: x.split(','), res_items)
-    #     res_items_end = list(
-    #         filter(lambda z: float(z[0]) > YOLO_SCORE_THRE,
-    #                res_item_coordinates))
+#         del res_items
+#         del res_item_coordinates
 
-    #     del res_items
-    #     del res_item_coordinates
+#         if not res_items_end:
+#             return []
 
-    #     if not res_items_end:
-    #         return []
+#         return [tuple(int(b) for b in a[1:]) for a in res_items_end]
 
-    #     return [tuple(int(b) for b in a[1:]) for a in res_items_end]
+#     def detect_object(self):
+#         """
+#         task: (video_id, video_path, frame_number, object_of_interest)
+#         """
+#         task = TaskQueue.get(block=True)
 
-    # def detect_object(self):
-    #     """
-    #     task: (video_id, video_path, frame_number, object_of_interest)
-    #     """
-    #     task = TaskQueue.get(block=True)
+#         _start_time = time.time()
 
-    #     _start_time = time.time()
+#         logger.debug(f'Task {task}')
 
-    #     logger.debug(f'Task {task}')
+#         yolo_channel = grpc.insecure_channel(YOLO_CHANNEL_ADDRESS)
+#         yolo_stub = det_yolov3_pb2_grpc.DetYOLOv3Stub(yolo_channel)
 
-    #     yolo_channel = grpc.insecure_channel(YOLO_CHANNEL_ADDRESS)
-    #     yolo_stub = det_yolov3_pb2_grpc.DetYOLOv3Stub(yolo_channel)
+#         # video_id
+#         _, video_name, video_path, = task[0], task[1], task[2]
+#         frame_num, object_name = task[3], task[4]
 
-    #     # video_id
-    #     _, video_name, video_path, = task[0], task[1], task[2]
-    #     frame_num, object_name = task[3], task[4]
+#         picked_frame: Frame = None
 
-    #     picked_frame: Frame = None
+#         try:
+#             picked_frame = db_session.query(Frame).filter(
+#                 Frame.name == str(frame_num)).filter(
+#                     Frame.processing_status ==
+#                     Status.Initialized).one_or_none()
+#         except MultipleResultsFound as m_err:
+#             logger.error(
+#                 f'Found too many frames with name {frame_num}: {m_err}')
 
-    #     try:
-    #         picked_frame = db_session.query(Frame).filter(
-    #             Frame.name == str(frame_num)).filter(
-    #                 Frame.processing_status ==
-    #                 Status.Initialized).one_or_none()
-    #     except MultipleResultsFound as m_err:
-    #         logger.error(
-    #             f'Found too many frames with name {frame_num}: {m_err}')
+#             picked_frame = db_session.query(Frame).filter(
+#                 Frame.name == str(frame_num)).filter(
+#                     Frame.processing_status == Status.Initialized).one()
 
-    #         picked_frame = db_session.query(Frame).filter(
-    #             Frame.name == str(frame_num)).filter(
-    #                 Frame.processing_status == Status.Initialized).one()
+#         except Exception as err:
+#             logger.error(err)
 
-    #     except Exception as err:
-    #         logger.error(err)
+#         if picked_frame:
+#             try:
+#                 picked_frame.processing_status = Status.Processing
+#                 db_session.commit()
+#             except Exception as err:
+#                 logger.error(err)
 
-    #     if picked_frame:
-    #         try:
-    #             picked_frame.processing_status = Status.Processing
-    #             db_session.commit()
-    #         except Exception as err:
-    #             logger.error(err)
+#             try:
+#                 v_name = '_'.join(video_name.split('.')[:-1])
+#                 image_dir = os.path.join(CONTROLLER_PICTURE_DIRECTORY, v_name)
+#                 if not os.path.exists(image_dir):
+#                     os.mkdir(image_dir)
 
-    #         try:
-    #             v_name = '_'.join(video_name.split('.')[:-1])
-    #             image_dir = os.path.join(CONTROLLER_PICTURE_DIRECTORY, v_name)
-    #             if not os.path.exists(image_dir):
-    #                 os.mkdir(image_dir)
+#                 img_name = os.path.join(image_dir, f'{frame_num}.jpg')
+#                 img_data = self.extract_one_frame(video_path, frame_num)
 
-    #             img_name = os.path.join(image_dir, f'{frame_num}.jpg')
-    #             img_data = self.extract_one_frame(video_path, frame_num)
+#                 logger.debug(f"Sending extracted frame {frame_num} to YOLO")
 
-    #             logger.debug(f"Sending extracted frame {frame_num} to YOLO")
+#                 img_payload = common_pb2.Image(data=img_data.tobytes(),
+#                                                height=img_data.shape[0],
+#                                                width=img_data.shape[1],
+#                                                channel=img_data.shape[2])
+#                 detected_objects = yolo_stub.DetFrame(
+#                     det_yolov3_pb2.DetFrameRequest(image=img_payload,
+#                                                    name=img_name,
+#                                                    cls=object_name))
 
-    #             img_payload = common_pb2.Image(data=img_data.tobytes(),
-    #                                            height=img_data.shape[0],
-    #                                            width=img_data.shape[1],
-    #                                            channel=img_data.shape[2])
-    #             detected_objects = yolo_stub.DetFrame(
-    #                 det_yolov3_pb2.DetFrameRequest(image=img_payload,
-    #                                                name=img_name,
-    #                                                cls=object_name))
+#                 boxes = self.get_bounding_boxes(detected_objects.res)
+#                 logger.debug(f"bounding box of {object_name}: {boxes}")
 
-    #             boxes = self.get_bounding_boxes(detected_objects.res)
-    #             logger.debug(f"bounding box of {object_name}: {boxes}")
+#                 setattr(picked_frame, 'processing_status', Status.Finished)
+#                 db_session.commit()
 
-    #             setattr(picked_frame, 'processing_status', Status.Finished)
-    #             db_session.commit()
+#                 if boxes:
+#                     temp_b = map(
+#                         lambda x: Element.coordinate_iterable_to_str(x), boxes)
+#                     ele_list = list(
+#                         map(lambda y: Element(object_name, y, picked_frame.id),
+#                             temp_b))
+#                     db_session.add_all(ele_list)
+#                     db_session.commit()
 
-    #             if boxes:
-    #                 temp_b = map(
-    #                     lambda x: Element.coordinate_iterable_to_str(x), boxes)
-    #                 ele_list = list(
-    #                     map(lambda y: Element(object_name, y, picked_frame.id),
-    #                         temp_b))
-    #                 db_session.add_all(ele_list)
-    #                 db_session.commit()
+#                     ImageQueue.put((img_name, img_data, boxes))
+#             except ImageDoesNotExistException:
+#                 picked_frame.processing_status = Status.Failed
+#                 logger.warning(f'frame {frame_num} cannot be processed')
+#                 db_session.commit()
+#             except Exception as err:
+#                 logger.error(
+#                     f'Working on task {task} but encounter error: {err}')
+#                 db_session.rollback()
 
-    #                 ImageQueue.put((img_name, img_data, boxes))
-    #         except ImageDoesNotExistException:
-    #             picked_frame.processing_status = Status.Failed
-    #             logger.warning(f'frame {frame_num} cannot be processed')
-    #             db_session.commit()
-    #         except Exception as err:
-    #             logger.error(
-    #                 f'Working on task {task} but encounter error: {err}')
-    #             db_session.rollback()
+#         db_session.remove()
 
-    #     db_session.remove()
+#         yolo_channel.close()
 
-    #     yolo_channel.close()
-
-    #     logger.debug(f'Take {time.time() - _start_time} m second to finish')
-
+#         logger.debug(f'Take {time.time() - _start_time} m second to finish')
 
 _CAMERA_STORAGE = {'jetson': {'host': "0.tcp.ngrok.io", 'port': "17171"}}
 
@@ -540,26 +537,26 @@ def grpc_serve():
     return server
 
 
-def detection_serve():
-    thread_list = []
+# def detection_serve():
+#     thread_list = []
 
-    for _ in range(FRAME_PROCESSOR_WORKER_NUM):
-        temp = FrameProcessor()
-        temp.setDaemon(True)
-        temp.start()
-        thread_list.append(temp)
+#     for _ in range(FRAME_PROCESSOR_WORKER_NUM):
+#         temp = FrameProcessor()
+#         temp.setDaemon(True)
+#         temp.start()
+#         thread_list.append(temp)
 
-    for _ in range(IMAGE_PROCESSOR_WORKER_NUM):
-        image_worker = ImageProcessor()
-        image_worker.setDaemon(True)
-        image_worker.start()
-        thread_list.append(image_worker)
+#     for _ in range(IMAGE_PROCESSOR_WORKER_NUM):
+#         image_worker = ImageProcessor()
+#         image_worker.setDaemon(True)
+#         image_worker.start()
+#         thread_list.append(image_worker)
 
-    logger.info("workers are runing")
+#     logger.info("workers are runing")
 
-    # NOTE should not join since we don't want the program got blocked here
-    # for _t in thread_list:
-    #     _t.join()
+#     # NOTE should not join since we don't want the program got blocked here
+#     # for _t in thread_list:
+#     #     _t.join()
 
 
 def draw_box(img, x1, y1, x2, y2):
@@ -662,7 +659,7 @@ if __name__ == '__main__':
 
     logger.info("Init threads")
 
-    detection_serve()
+    # detection_serve()
 
     _server = grpc_serve()
     _server.wait_for_termination()
