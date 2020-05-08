9fa40720d003c2ea58077ef6dea7ef548b4aeb21
Bryan <bryanjw01@gmail.com>
2020-05-08 15:23:43 -0400

    updated version of website and web server

--- web/static/js/extra_stuff.js
+++ web/static/js/extra_stuff.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
     let video_name;
-    let obj_name;
     let duration;
     let timestamp = 0;
     let camera_name;
@@ -9,9 +8,144 @@ document.addEventListener('DOMContentLoaded', function(){
     let video_URL;
     let score_URL;
     let image_URL;
+    let index = 0;
+    let id = null;
+    let elem = null;
+
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+    };
+
+     document.getElementById("video1").addEventListener("click", function() {
+            video(0);
+        }, false);
+
+     document.getElementById("video2").addEventListener("click", function() {
+            video(1);
+        }, false);
+
+     document.getElementById("video3").addEventListener("click", function() {
+            video(2);
+        }, false);
+
+     document.getElementById("video4").addEventListener("click", function() {
+            video(3);
+        }, false);
+
+    function video(a){
+        vid = a;
+        index = a;
+        document.getElementById('slider_0').style.display = "block";
+        document.getElementById('vidtitle').style.display = "block";
+        document.getElementById('video').style.display = "block";
+        document.getElementById('video_tab').style.display = "block";
+        console.log(video_URL);
+        slider.max = duration[index];
+        console.log(duration[index]);
+        if(document.getElementById('videodisplay').src === video_URL[a]){
+
+        }
+        else {
+            document.getElementById('videodisplay').src = video_URL[a];
+        }
+    }
+
+
+    document.getElementById("sort_time").addEventListener("click", function() {
+            sort_time();
+        }, false);
+
+
+    document.getElementById("sort_confidence").addEventListener("click", function() {
+            sort_confidence();
+        }, false);
+
+    document.getElementById("query").addEventListener("click", function() {
+            call_diva_2();
+        }, false);
+
     document.getElementById("vid_req").addEventListener("click", function() {
             request_videos();
+            // slider.max = "100";
+            // update_progress_bar(parseInt(slider.max));
             }, false);
+
+    function array(num){
+        document.getElementById('videodisplay').currentTime = num;
+    }
+
+    function sort_confidence(){
+        let temp_array = [];
+        let child = document.getElementById('results').children;
+        console.log(child.length);
+         for (let i = 0; i < child.length; i+=1){
+              temp_array.push([child[i].src , child[i].getAttribute('time')
+                  , child[i].getAttribute('confidence')])
+         }
+         temp_array = temp_array.sort(function(a, b){return b[2]-a[2]});
+           // if (document.getElementById('results').childNodes.length > 0) {
+            child = document.getElementById('results').lastElementChild;
+            while (child) {
+                document.getElementById('results').removeChild(child);
+                child = document.getElementById('results').lastElementChild;
+            }
+         // }
+
+         for (let i = 0; i < temp_array.length; i++){
+                let img = new Image();
+                img.src = temp_array[i][0];
+                img.width = 400;
+                img.height = 300;
+                img.setAttribute("time", temp_array[i][1]);
+                img.setAttribute("confidence", temp_array[i][2]);
+                img.title = 'Confidence Score: ' + temp_array[i][2] + '\n Timestamp: ' + temp_array[i][1];
+                // img.addEventListener('click', function () {
+                //     array(temp_array[i][1]);
+                // }, false);
+                document.getElementById('results').appendChild(img);
+        }
+         console.log(temp_array);
+    }
+
+
+    function sort_time(){
+        let temp_array = [];
+        let child = document.getElementById('results').children;
+         for (let i = 0; i < child.length; i+=1){
+              temp_array.push([child[i].src , child[i].getAttribute('time')
+                  , child[i].getAttribute('confidence')])
+         }
+         temp_array = temp_array.sort(function(a, b){return a[1]-b[1]});
+
+         // if (document.getElementById('results').childNodes.length > 0) {
+             child = document.getElementById('results').lastElementChild;
+            while (child) {
+                document.getElementById('results').removeChild(child);
+                child = document.getElementById('results').lastElementChild;
+            }
+         // }
+
+         for (let i = 0; i < temp_array.length; i++){
+                let img = new Image();
+                img.src = temp_array[i][0];
+                img.width = 400;
+                img.height = 300;
+                img.setAttribute("time", temp_array[i][1]);
+                img.setAttribute("confidence", temp_array[i][2]);
+                img.title = 'Confidence Score: ' + temp_array[i][2] + '\n Timestamp: ' + temp_array[i][1];
+                console.log(temp_array[i][1]);
+                // img.addEventListener('click', function () {
+                //     array(temp_array[i][1]);
+                // }, false);
+                document.getElementById('results').appendChild(img);
+        }
+         console.log(temp_array);
+    }
+
 
     function request_videos(){
         let computeReturn = $.ajax({
@@ -19,54 +153,121 @@ document.addEventListener('DOMContentLoaded', function(){
                 contentType: 'application/json',
                 url: "/request_video",
         }).done(function (computeReturn) {
+            document.getElementById('vid_req').style.display = "none";
+            document.getElementById('obj_dropdown').style.display = "block";
+            document.getElementById('img_1').style.display = "block";
+             document.getElementById('img_2').style.display = "block";
+             document.getElementById('img_3').style.display = "block";
+             document.getElementById('img_4').style.display = "block";
             console.log(computeReturn);
             video_name = computeReturn['video'];  //
             duration = computeReturn['frame'];  // duration of video in seconds. Videos being processed are 30 fps.
             video_URL = computeReturn['video_URL'];  //
-
+            console.log(duration);
             camera_name = computeReturn['camera_name'];
             camera_address = computeReturn['camera_address'];
-            //
-            call_diva_2();
+            console.log(typeof(duration), duration);
         });
     }
 
     function call_diva_2(){
-        console.log("good");
+        // console.log("good");
+        document.getElementById('work').style.display = "block";
+        document.getElementById('workimage').style.display = "block";
+        document.getElementById('demo_tab').style.display = "block";
+        console.log(duration[index]);
+        // console.log(slider.max, typeof(slider.max));
+        document.getElementById("query").disabled = true;
+        update_progress_bar(parseInt(duration[index]) / 30);
         let computeReturn = $.ajax({
             method: "POST",
             contentType: 'application/json',
             dataType: "json",
-            data:JSON.stringify({'object':'motorbike', 'video': video_URL, 'camera_name': camera_name, 'timestamp': timestamp,
-            'offset': offset, 'camera_address': camera_address
+            data:JSON.stringify({'object':obj, 'video': video_URL[index], 'camera_name': camera_name[index], 'timestamp': timestamp,
+            'offset': slider.value, 'camera_address': camera_address[index]
             }),
             url: "/display",
          }).done(function (computeReturn) {
-             console.log(computeReturn);
-             refresh();
-             window.setTimeout(refresh,3000);
+             image_URL = computeReturn['image_url'];
+             score_URL = computeReturn['conf'];
+             console.log(image_URL);
+             console.log(score_URL);
+             clearInterval(id);
+             elem.textContent = "Processed: " + 100 + "%";
+             elem.style.width = 100 + "%";
+             if (document.getElementById('results').childNodes.length > 0) {
+                let child = document.getElementById('results').lastElementChild;
+                while (child) {
+                    document.getElementById('results').removeChild(child);
+                    child = document.getElementById('results').lastElementChild;
+                }
+             }
+             display_images();
+             document.getElementById("query").disabled = false;
         });
     }
 
 
-    function refresh(){
-        console.log("refresh");
-        let computeReturn = $.ajax({
-            method: "POST",
-            contentType: 'application/json',
-            dataType: "json",
-            data:JSON.stringify({ 'video': video_name, //
-                'frame':duration,
-                'video_URL':video_URL,  //
-                'camera_name': camera_name,
-                'camera_address':camera_address
-            }),
-            url: "/refresh",
-         }).done(function (computeReturn) {
 
+    function display_images() {
 
-        });
+        for (let i = 0; i < image_URL.length; i += 1) {
+            // let img = new Image();
+            let img = document.createElement('img');
+            img.src = image_URL[i];
+
+            img.width = 400;
+            img.height = 300;
+            let time = image_URL[i].split('/');
+            time = time[time.length - 1];
+            time = time.split('.');
+            time = time[0];
+            console.log(time);
+            img.title = 'Confidence Score: ' + score_URL[time]['0'] + '\n Timestamp: ' + time;
+            img.setAttribute("time", parseInt(time));
+            img.setAttribute("confidence", score_URL[time]['0']);
+            // img.addEventListener('click', function () {
+            //     array(time);
+            // }, false);
+            document.getElementById('results').appendChild(img);
+        }
     }
 
+    function reset_progress_bar(){
+        let elem = document.getElementById("myBar");
+        elem.style.width =  "0%";
+        elem.setAttribute("aria-valuenow", 0);
+    }
+
+
+    function update_progress_bar(total) {
+        console.log(total, typeof total);
+        reset_progress_bar();
+        elem = document.getElementById("myBar");
+        let width = 0;
+        let proc = 0;
+        id = setInterval(frame, 920);
+        let temp_index = 0;
+        let temp = 0;
+        function frame() {
+            console.log(width);
+            if (width >= 100 && proc === total && temp_index % 10 === 0) {
+                console.log("done");
+                clearInterval(id);
+            }
+            else {
+                width = ((width * 0.01 * total)+1) / total * 100;
+                if(width > 100){
+                    width = 99;
+                }
+                console.log(width);
+                elem.style.width = width + "%";
+                proc = parseInt(total * width * 0.01, 10);
+                temp += 1;
+                console.log(proc, total);
+                elem.textContent = Math.floor(width) + "%";
+            }
+        }
+    }
 
 },false);
--- web/web_server.py
+++ web/web_server.py
@@ -1,12 +1,17 @@
 from __future__ import print_function
-import logging
+# import logging
 import grpc
 import server_diva_pb2
 import server_diva_pb2_grpc
 from variables import DIVA_CHANNEL_ADDRESS
 from flask import Flask, render_template, request
-from flask import jsonify, send_from_directory
-import web.query as query
+from flask import jsonify,  send_from_directory
+import json
+import os
+from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
+# import grpc-tools
+import requests
+
 
 
 OUTPUT_DIR = './result/retrieval_imgs/'
@@ -24,54 +29,144 @@ def index():
 def download_file(filename):
     return send_from_directory(OUTPUT_DIR, filename, as_attachment=True)
 
+@app.route('/request_video',methods=['GET', 'POST'])
+def request_video():
+    print('gojsapeogj')
+    frames = list()
+    name = list()
+    camera_name = list()
+    camera_address = list()
+    video_url = list()
+    with grpc.insecure_channel(DIVA_CHANNEL_ADDRESS) as channel:
+        stub = server_diva_pb2_grpc.server_divaStub(channel)
+        response = stub.get_videos(google_dot_protobuf_dot_empty__pb2.Empty())
+        print(response)
+    # response = response.videos[2]
+    for i in range(4):
+        frames.append(response.videos[i].frames)
+        name.append(response.videos[i].name)
+        camera_name.append(response.videos[i].camera.name)
+        camera_address.append(response.videos[i].camera.address)
+        video_url.append(response.videos[i].video_url)
+    print(frames)
+    return jsonify(frame=frames, video=name, image_URL='',
+                   video_URL=video_url, score_URL='', camera_address=camera_address, camera_name=camera_name)
+
+
 
 @app.route('/display', methods=['GET', 'POST'])
 def display():
+    print("ok")
     obj = request.json['object']
     video = request.json['video'].split('/')[-1]
-    print(obj, video)
-    logging.basicConfig()
+    cam_name = request.json['camera_name']
+    cam_address = request.json['camera_address']
+    start = request.json['timestamp']
+    end = request.json['offset']
+    print(end)
+    print(obj, video, cam_name, cam_address, start, end)
     with grpc.insecure_channel(DIVA_CHANNEL_ADDRESS) as channel:
         stub = server_diva_pb2_grpc.server_divaStub(channel)
-        #  response = stub.request_frame_path(server_diva_pb2.query_statement(name='request directory'))
-        #  FIXME: replace with other function call
-        _ = stub.detect_object_in_video(server_diva_pb2.object_video_pair(object_name=obj,  video_name=video))
-    return jsonify(file='good')
+        response = stub.process_video(server_diva_pb2.common__pb2.VideoRequest(timestamp=0, offset=int(end),
+                                                                               video_name=video, object_name=obj,
+                                                                               camera={'name': cam_name, 'address': cam_address}))
+        response = stub.get_video(server_diva_pb2.common__pb2.VideoRequest(timestamp=0, offset=int(end),
+                                                                               video_name=video,
+                                                                               object_name='motorbike',
+                                                                               camera={'name': cam_name,
+                                                                                       'address': cam_address}))
+    print(response)
+    image_list = list()
+    images_URL = response.images_url
+    conf_URL = response.score_file_url
+    print(conf_URL)
+    while True:
+        try:
+
+            confidence = requests.get(conf_URL)
+            confidence = json.loads(confidence.content)
+        except:
+            continue
+
+        break
+    for i in confidence:
+        image_list.append(images_URL + i + ".jpg")
+    print(image_list, confidence)
+    return jsonify(image_url=image_list, conf=confidence)
 
 
-@app.route('/retrieve', methods=['GET', 'POST'])
-def retrieve_frames():
-    time = list()
-    video_name = request.json['video'].split('/')[-1]
-    print(video_name)
-    name, status = query.request_frames(video_name)
-    for i in name:
-        time.append(str(int(i) // 10))
-    name = ','.join(name)
-    time = ','.join(time)
-    print(time)
-    if not status:
-        print('not finished')
-        return jsonify(file=name, t=time, status=False)
-    print('finished')
-    return jsonify(file=name, t=time, status=True)
+# @app.route('/retrieve', methods=['GET', 'POST'])
+# def retrieve_frames():
+#     time = list()
+#     video_name = request.json['video'].split('/')[-1]
+#     print(video_name)
+#     name, status = query.request_frames(video_name)
+#     for i in name:
+#         time.append(str(int(i) // 10))
+#     name = ','.join(name)
+#     time = ','.join(time)
+#     print(time)
+#     if not status:
+#         print('not finished')
+#         return jsonify(file=name, t=time, status=False)
+#     print('finished')
+#     return jsonify(file=name, t=time, status=True)
+
+@app.route('/refresh', methods=['GET', 'POST'])
+def refresh():
+    frames = request.json['frame']
+    name = request.json['video']
+    camera_name = request.json['camera_name']
+    camera_address = request.json['camera_address']
+    video_url = request.json['video_URL']
+    print(frames, name, camera_name, camera_address, video_url)
+    # print('ok')
+    with grpc.insecure_channel(DIVA_CHANNEL_ADDRESS) as channel:
+        stub = server_diva_pb2_grpc.server_divaStub(channel)
+        response = stub.get_video(timestamp=0, offset=1000, video_name="example.mp4", object_name="motorbike", camera={'name': "jetson", 'address': "3.134.196.116:17911"})
+        print(response)
+    images_URL = response.images_url
+    conf_URL = response.score_file_url
+    print(conf_URL, images_URL)
+    # with uurlopen(conf_URL) as conf:
+    #     #     confidence = json.loads(conf.read())
+    confidence = 0
+    image_name = images_URL.split('/')[-1].split('.')
+    image_name = image_name[0]
+    return jsonify(img=images_URL, conf=confidence, image_name=image_name)
+
+@app.route('/temp', methods=['GET', 'POST'])
+def temp():
+    obj = request.json['object']
+    print(obj)
+    confidence_file = obj + '_score.txt'
+    with open(os.path.join('./web/static/test1/img', confidence_file), 'r') as f:
+        conf = f.read()
+    # print(conf)
+    confidence = json.loads(conf)
+    print(confidence)
+    images = os.listdir(os.path.join('./web/static/test1/img', obj))
+    videos = os.listdir(os.path.join('./web/static/test1/video', obj))
+    images.sort()
+    videos.sort()
+    print(len(images))
+    return jsonify(img=images, vid=videos, conf=confidence, obj=obj)
 
 
-@app.route('/ret_num', methods=['GET', 'POST'])
-def ret_num():
-    time = list()
-    video_name = request.json['video'].split('/')[-1]
-    num_processed, total = query.num_frames(video_name)
-    return jsonify(processed=num_processed, total=total)
+# @app.route('/ret_num', methods=['GET', 'POST'])
+# def ret_num():
+#     time = list()
+#     video_name = request.json['video'].split('/')[-1]
+#     num_processed, total = query.num_frames(video_name)
+#     return jsonify(processed=num_processed, total=total)
 
 
-@app.route('/coordinates', methods=['GET', 'POST'])
-def retrieve_coordinates():
-    video = request.json['video'].split('/')[-1]
-    coordinates = query.request_coordinates(video)
-    return jsonify(coordinates)
+# @app.route('/coordinates', methods=['GET', 'POST'])
+# def retrieve_coordinates():
+#     video = request.json['video'].split('/')[-1]
+#     coordinates = query.request_coordinates(video)
+#     return jsonify(coordinates)
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 10000)
-
